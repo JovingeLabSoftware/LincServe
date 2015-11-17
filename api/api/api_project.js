813821c864785bffbe067598cda3bb68c9cf5e03
Eric J. Kort <ericjkort@gmail.com>
2015-11-17 21:44:48 +0000

    API documentation added

--- api/api/api_project.js
+++ api/api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-11-17T21:36:29.918Z",
+    "time": "2015-11-17T21:43:36.505Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
