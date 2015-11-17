d9936df89203c4197fa078f2fa14d210eeaac73f
Eric J. Kort <ericjkort@gmail.com>
2015-11-17 21:32:16 +0000

    API documentation added

---
+++ api/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "title": "LINCS REST API Browser",
+  "url": "https://api.github.com/v1",
+  "name": "CouchLincs",
+  "version": "1.0.0",
+  "description": "A high performance (hopefully) LINCS data server including Node.js server, couchbase backend, and R package for ETL",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-11-17T20:56:10.393Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
