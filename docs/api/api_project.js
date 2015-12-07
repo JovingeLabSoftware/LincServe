e0f5ce36e3561332bf806e6895d9925fd0eb4871
Eric J. Kort <ericjkort@gmail.com>
2015-12-07 22:22:46 +0000

    closes #8. prior commits also implemented end points for creating instance and perturbation docs.  see API docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-12-07T20:45:08.368Z",
+    "time": "2015-12-07T22:22:31.068Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
