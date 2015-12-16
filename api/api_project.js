f10a9683b24d17fac1a484114100ca2c9528c8e2
Eric J. Kort <ericjkort@gmail.com>
2015-12-16 15:18:46 +0000

    Documentation update

--- api/api_project.js
+++ api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-12-16T15:18:37.579Z",
+    "time": "2015-12-16T15:18:45.943Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
